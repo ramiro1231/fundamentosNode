6b853b4804eb33fbf575f3b06534ddae71dc6b9a
Ramiro Alvarez <ramiro89011@gmail.com>
2020-04-23 22:53:49 -0500

    Global

---
+++ modulos/globales.js
@@ -0,0 +1,16 @@
+/*let i =0;
+let intervalo = setInterval(() => {
+    console.log('Hola')
+    if ( i === 3) {
+        clearInterval(intervalo);
+    }
+    i++;
+}, 1000);
+
+setImmediate(() => {
+    console.log('Hola')
+})*/
+
+console.log(__filename);
+
+globalThis.miVariable = 'valor';
