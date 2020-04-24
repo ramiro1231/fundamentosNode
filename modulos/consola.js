372f38d2054947a63ab036e1f429e76c2e754a61
Ramiro Alvarez <ramiro89011@gmail.com>
2020-04-23 23:19:17 -0500

    Consola

---
+++ modulos/consola.js
@@ -0,0 +1,44 @@
+/*console.log('algo');
+
+console.info('algo');
+
+console.error('algo');
+
+console.warn('algo');*/
+/*
+var tabla = [
+    {
+        a:1,
+        b: 'z'
+    },
+    {
+        a: 2,
+        b: 'Otra'
+    }
+]
+console.table(tabla);
+
+*/
+/*
+function function1 () {
+    //
+    console.group('funcion 1');
+    console.log('funcion 1 1 1')
+    function2();
+    console.groupEnd('funcion 1');
+}
+
+function function2 () {
+    //
+    console.group('funcion 2');
+    console.log('funcion 2');
+    console.groupEnd('funcion 2');
+}
+
+console.log('Empezamos')
+
+function1();*/
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
