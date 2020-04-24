62b8226867781b44ff92c8633bc945477cbc73ae
Ramiro Alvarez <ramiro89011@gmail.com>
2020-04-23 21:29:49 -0500

    Promesas

---
+++ async/promises.js
@@ -0,0 +1,41 @@
+
+
+
+function hola (nombre) {
+     return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            console.log('Hola ' + nombre)
+            resolve(nombre);
+        }, 1500);
+     });
+}
+
+function hablar(nombre) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Bla bla bla bla... ')
+            resolve(nombre);
+        }, 1000);
+    })
+}
+function adios(nombre, otroCallback) {
+    return new Promise ( (resolve, reject) => {
+        setTimeout(() => {
+            console.log('Adios ' + nombre)
+            resolve(nombre);
+        }, 1000);
+    });
+
+}
+
+console.log('Iniciado el proceso ');
+hola('Ramiro')
+    .then(hablar)
+    .then ( adios)
+    .then( (nombre) => {
+        console.log('Terminado el proceso ');
+    })
+    .catch( error => {
+        console.error ('Ha habido un error: ');
+        console.error(error);
+    })
