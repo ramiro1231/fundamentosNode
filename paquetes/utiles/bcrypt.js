9bfd8a761f10e70ebc3aefdef56d27d2907756b8
Ramiro Alvarez <ramiro89011@gmail.com>
2020-04-25 00:23:25 -0500

    modulos utiles

---
+++ paquetes/utiles/bcrypt.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+
+const password = '1234Segura!';
+
+bcrypt.hash(password, 5, function(err, hash) {
+    //
+    console.log(hash);
+
+    bcrypt.compare('password', hash, function (err, res) {
+        console.log(res);
+    })
+})
+
