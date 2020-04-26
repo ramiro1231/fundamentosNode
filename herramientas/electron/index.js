f36ba4cca7bb345cef535b6c413e464412b317a0
Ramiro Alvarez <ramiro89011@gmail.com>
2020-04-25 22:30:38 -0500

    electron

---
+++ herramientas/electron/index.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', createWindow);
+
+function createWindow() {
+    mainWindow = new BrowserWindow( {
+        width: 800,
+        height: 600,
+        
+    });
+
+    mainWindow.loadFile('index.html');
+};
+
